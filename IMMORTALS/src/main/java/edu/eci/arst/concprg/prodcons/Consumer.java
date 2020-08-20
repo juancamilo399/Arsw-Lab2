23b5aab96ca98ce51e451725540e193922ec744c
NicolasAguileraa9906 <nicolasaguileracontreras@gmail.com>
2020-08-20 12:45:51 -0500

    codigo parte 1 e imagenes

--- IMMORTALS/src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
+++ IMMORTALS/src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
@@ -6,29 +6,41 @@
 package edu.eci.arst.concprg.prodcons;
 
 import java.util.Queue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author hcadavid
  */
-public class Consumer extends Thread{
-    
+public class Consumer extends Thread {
+
     private Queue<Integer> queue;
-    
-    
-    public Consumer(Queue<Integer> queue){
-        this.queue=queue;        
+
+
+    public Consumer(Queue<Integer> queue) {
+        this.queue = queue;
     }
-    
+
     @Override
     public void run() {
         while (true) {
-
-            if (queue.size() > 0) {
-                int elem=queue.poll();
-                System.out.println("Consumer consumes "+elem);                                
+            synchronized (this) {
+                while (queue.size() == 0) {
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                queue.poll();
+                notify();
             }
-            
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
 }
--- IMMORTALS/src/main/java/edu/eci/arst/concprg/prodcons/Producer.java
+++ IMMORTALS/src/main/java/edu/eci/arst/concprg/prodcons/Producer.java
@@ -31,17 +31,23 @@ public class Producer extends Thread {
     @Override
     public void run() {
         while (true) {
-
-            dataSeed = dataSeed + rand.nextInt(100);
-            System.out.println("Producer added " + dataSeed);
-            queue.add(dataSeed);
-            
+            synchronized (this) {
+                while (queue.size() == this.stockLimit) {
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                dataSeed = dataSeed + rand.nextInt(100);
+                System.out.println("Producer added " + dataSeed);
+                queue.add(dataSeed);
+            }
             try {
-                Thread.sleep(1000);
+                Thread.sleep(1);
             } catch (InterruptedException ex) {
                 Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
             }
-
         }
     }
 }
